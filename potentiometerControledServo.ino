48073dfabcc9ba7c1bb836b3b54b2ab4b7457960
pyright <camposmike92@gmail.com>
2017-05-10 00:19:16 -0500

    Update potentiometerControledServo.ino

--- potentiometerControledServo.ino
+++ potentiometerControledServo.ino
@@ -1,27 +1,19 @@
-/*
- Controlling a servo position using a potentiometer (variable resistor)
- by Michal Rinott <http://people.interaction-ivrea.it/m.rinott>
-
- modified on 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Knob
-*/
-
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+// create servo object to control a servo
+Servo myservo;  
 
-int potpin = 0;  // analog pin used to connect the potentiometer
-int val;    // variable to read the value from the analog pin
+int potpin = 0;  // analog pin connects the potentiometer
+int val;    // variable reads value from the analog pin
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(9);  // attaches servo on pin 9 to servo object
 }
 
 void loop() {
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-  myservo.write(val);                  // sets the servo position according to the scaled value
-  delay(15);                           // waits for the servo to get there
+  val = analogRead(potpin);           // reads value of potentiometer, 0 to 1023
+  val = map(val, 0, 1023, 0, 180);     // scale to use with the servo, between 0 to 180
+  myservo.write(val);                  // sets servo position according to scaled value
+  delay(15);                           // waits for servo to get there
 }
 
